b45e2dd52f507866c8cfb068211549bef18963ee
akhatri-tpr <55406923+akhatri-tpr@users.noreply.github.com>
2024-05-27 17:54:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate with CodePipeline!');
+  response.end('Hello.This is Ajay Khatri and This app tested with Node Server App deployed on ECS Fargate with CodePipeline! Thanks');
   
 };
 var www = http.createServer(handleRequest);
